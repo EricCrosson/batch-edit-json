d0a6fcbab0f276d0ba20f454e46ce71bc4d4a928
Eric Crosson <eric.s.crosson@utexas.edu>
2019-10-04 08:32:44 -0500

    feat: read files from disk

--- src/batch-edit-json.ts
+++ src/batch-edit-json.ts
@@ -5,10 +5,16 @@
 
 namespace debug {
     export const options = require('debug')('bej:options')
+    export const io = require('debug')('bej:io')
 }
 
 import { docopt } from 'docopt'
 import { version } from './version'
+import globby from 'globby'
+import matcher from 'matcher'
+import path from 'path'
+import { Maybe } from 'purify-ts/Maybe'
+import { Either, Left, Right } from 'purify-ts/Either'
 
 
 interface UserOptions {
@@ -16,16 +22,18 @@ interface UserOptions {
     "--version": boolean;
     "-a": boolean;
     "-r": boolean;
+    "-e": boolean;
     "-v": boolean;
     "<key>": string;
     "<object>": string;
+    "<exclude_path>": string;
     "<path>": string[];
 }
 
 
 const docstring: string = `
 Usage:
-    batch-edit-json [-v] [-r <key>] [-a <object>] <path>...
+    batch-edit-json [-v] [-r <key>] [-a <object>] [-e <exclude_path>] <path>...
     batch-edit-json --help | --version
 
 -r               Remove key from matching paths
@@ -35,10 +43,52 @@ Usage:
 --version        Show version number
 `
 
-const options: UserOptions = docopt(docstring, {
-    help: true,
-    version: version,
-    exit: true
-})
+function tryCatch(func: Function): Either<any, Error> {
+    try {
+        return Right(func())
+    } catch (error) {
+        return Left(error)
+    }
+}
 
-debug.options(JSON.stringify(options, null, 4))
+function readFile(file: string) {
+    debug.io(`Parsing ${file}`)
+    return tryCatch(() => require(file))
+}
+
+function main() {
+
+    const options: UserOptions = docopt(docstring, {
+        help: true,
+        version: version,
+        exit: true
+    })
+    debug.options(JSON.stringify(options, null, 4))
+
+    const paths = options['<path>']
+        .reduce(
+            (acc: string[], path) => [...acc, ...globby.sync(path)],
+            []
+        )
+        .reduce(
+            (acc: string[], p) => options['-e'] && matcher.isMatch(p, options['<exclude_path>'])
+                ? acc
+                : [...acc, p],
+            []
+        )
+        .map(p => [process.cwd(), p].join(path.sep))
+    debug.io(`Matching paths:`, JSON.stringify(paths, null, 4))
+
+    for (const file of paths) {
+        readFile(file)
+            // .map(data => {
+
+            // })
+            .bimap(
+                console.error,
+                data => debug.io(file, JSON.stringify(data, null, 4))
+            )
+    }
+}
+
+main()
